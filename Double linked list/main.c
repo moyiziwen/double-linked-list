7c5641a1a4ebeb3b92ec4fcb2cd7eefcd81552ff
luo <luo@DESKTOP-0A12N72>
2021-01-13 17:33:24 +0800

    未完善

--- Double linked list/main.c
+++ Double linked list/main.c
@@ -3,5 +3,11 @@
 
 
 int main() {
-
+	double_list *L = (double_list*)malloc(sizeof(double_list));
+	int n;
+	printf_s("¸öÊý:");
+	scanf_s("%d", &n);
+	L = init_list(n);
+	prinr_list(L);
+	system("pause");
 }
--- Double linked list/operation.h
+++ Double linked list/operation.h
@@ -1,2 +1,42 @@
 #include<stdio.h>
+#include<stdlib.h>
 
+typedef struct double_list {
+	int ele;
+	struct double_list *next;
+	struct double_list *prior;
+}double_list;
+
+double_list *init_list(int n) {
+	double_list *p = (double_list*)malloc(sizeof(double_list));
+	double_list *head = p;
+	head = NULL;
+	for (int i = 0; i < n; i++)
+	{
+		double_list *a = (double_list*)malloc(sizeof(double_list));
+		scanf_s("%d", &a->ele);
+		a->next = NULL;
+		a->prior = head;
+		head->next = a;
+	}
+	return p;
+}
+
+
+void prinr_list(double_list *L) {
+	double_list *a = L;
+	a = a->next;
+	for (int i = 0; a!=NULL; i++)
+	{
+		printf_s("%d ", a->ele);
+		a = a->next;
+	}
+	printf_s("\n"); 
+	a = a->prior;
+	for (int i = 0; a != NULL; i++)
+	{
+		printf_s("%d ", a->ele);
+		a = a->prior;
+	}
+	printf_s("\n");
+}
