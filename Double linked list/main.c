1730278165289b3c3277df7d51ad5f110af3ed30
luo <luo@DESKTOP-0A12N72>
2021-01-14 20:17:02 +0800

    不完善

--- Double linked list/main.c
+++ Double linked list/main.c
@@ -3,11 +3,15 @@
 
 
 int main() {
-	double_list *L = (double_list*)malloc(sizeof(double_list));
+	double_list *L;
 	int n;
 	printf_s("¸öÊı:");
 	scanf_s("%d", &n);
 	L = init_list(n);
 	prinr_list(L);
+	insert_list(L);
+	prinr_list(L);
+
+
 	system("pause");
 }
--- Double linked list/operation.h
+++ Double linked list/operation.h
@@ -10,33 +10,97 @@ typedef struct double_list {
 double_list *init_list(int n) {
 	double_list *p = (double_list*)malloc(sizeof(double_list));
 	double_list *head = p;
-	head = NULL;
-	for (int i = 0; i < n; i++)
+	head->prior = NULL;
+	head->next = NULL;
+	//printf_s("元节点值：\n");
+	scanf_s("%d", &head->ele);
+	//printf_s("\n其余节点：\n");
+	for (int i = 0; i < n - 1; i++)
 	{
 		double_list *a = (double_list*)malloc(sizeof(double_list));
 		scanf_s("%d", &a->ele);
 		a->next = NULL;
-		a->prior = head;
+		a->prior = NULL;
 		head->next = a;
+		a->prior = head;
+		head = head->next;
 	}
 	return p;
 }
 
+double_list *insert_list(double_list *L) {
+	double_list *a = L;
+	double_list *b = L;
+	int q = 0;
+	while (a != NULL)
+	{
+		a = a->next;
+		q++;
+	}
+	double_list *a1 = L;
+	double_list *c = (double_list*)malloc(sizeof(double_list));
+	c->next = NULL;
+	c->prior = NULL;
+	int x, n, i = 1;
+	printf_s("插入-值 位置:");
+	scanf_s("%d %d", &x, &n);
+	c->ele = x;
+	if (n > q + 1 || n < 1)
+	{
+		return L;
+	}
+	if (n == 1)
+	{
+		a1->prior = c;
+		c->next = a1;
+		a1 = a1->prior;
+		return L;
+	}
+
+	if (n == q + 1)
+	{
+		while (a1 != NULL)
+		{
+			b = a1;
+			a1 = a1->next;
+		}
+		a1 = b;
+		a1->next = c;
+		c->prior = a1;
+		a1 = a1->next;
+		return L;
+	}
+
+	double_list *a2 = L;
+	if (n > 1 & n < q + 1)
+	{
+		for (int i = 2; i < n; i++)
+		{
+			a2 = a2->next;
+		}
+		c->next = a2->next;
+		c->prior = a2;
+		a2->next->prior = c;
+		a2->next = c;
+		a2 = a2->next;
+	}
+}
+
+
 
 void prinr_list(double_list *L) {
-	double_list *a = L;
-	a = a->next;
-	for (int i = 0; a!=NULL; i++)
-	{
-		printf_s("%d ", a->ele);
-		a = a->next;
-	}
-	printf_s("\n"); 
-	a = a->prior;
+	double_list *a = L, *b = L;
 	for (int i = 0; a != NULL; i++)
 	{
 		printf_s("%d ", a->ele);
-		a = a->prior;
+		b = a;
+		a = a->next;
+	}
+	printf_s("\n");
+	for (int i = 0; b != NULL; i++)
+	{
+		printf_s("%d ", b->ele);
+		b = b->prior;
 	}
 	printf_s("\n");
 }
